5d68e6c760279901624285d2cf2df272511a24e5
= <=>
2014-03-22 03:03:50 -0700

    cleanup, added static index.html

--- server.js
+++ server.js
@@ -3,16 +3,15 @@
 	var http     = require('http');
 	var mongoose = require('mongoose'); 			
 
-	// configuration =================
 
 	mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'); 
 
 	app.configure(function() {
-		app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-		app.use(express.logger('dev')); 						// log every request to the console
-		app.use(express.bodyParser()); 							// pull information from html in POST
+		app.use(express.static(__dirname + '/build')); 		
+		app.use(express.logger('dev')); 						
+		app.use(express.bodyParser()); 						
   	app.use(express.cookieParser());
-		app.use(express.methodOverride()); 						// simulate DELETE and PUT
+		app.use(express.methodOverride()); 				
 	});
 
 	
