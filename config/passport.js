2b2028891409d5d37391602fd6b67fef408a1246
8legged <jason.niemi@live.com>
2014-03-28 14:22:44 -0700

    display user and secure profile page

--- config/passport.js
+++ config/passport.js
@@ -66,6 +66,32 @@ module.exports = function(passport) {
       });
     });
   }));
+
+  //  ================
+  //  local login
+  //  ================
+  passport.use('local-login', new LocalStrategy({
+    // by default, local strategy uses username and password, we will override with email
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true // allows us to pass back the entire request to the callback
+  },
+  function(req, email, password, done) { // callback with email and password from our form
+    // find a user whose email is the same as the form's email
+    User.findOne({ 'local.email' : email }, function(err, user) {
+      // if there are any errors, return them before anything else
+      if (err)
+        return done(err);
+      // if no user is found, return the message
+      if (!user)
+        return done(null, false, req.flash('loginMessage', 'No user found')); // req.flash is the way to set flashdata using connect-flash
+      // if the user is found but the password is wrong
+      if (!user.validPassword(password))
+        return done(null, false, req.flash('loginMessage', 'Sorry, wrong password')); // create the loginMessage and save it to session as flashdata
+      // all is well, return successful user
+      return done(null, user);
+    });
+  }));
 };
 
 
