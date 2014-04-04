b7de52f0a006abcd3077e50d04097a6560b89353
bl00m <tguchi3@gmail.com>
2014-04-04 15:03:45 -0700

    limit search to 3

--- api/routes/drinkRoutes.js
+++ api/routes/drinkRoutes.js
@@ -5,24 +5,28 @@ exports.findById = function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   var ingredient = req.params.ingredient;
   var tag  = req.params.tag;
-  Drink.find({ alcohol_tag: { $regex: ingredient, $options: 'i'}, tag: tag}, function(err, responseDrink) {
-    if(err) {
-      res.send(500, {'error': err});
-    } else if (responseDrink.length === 0){
-      Drink.count({}, function(err, count) {
-        var rand = Math.ceil(Math.random() * count);
-        console.log("random drink");
-        Drink.findOne({random: rand}, function(err, drink) {
-          if (err) { res.send({error: err});}
-          else {
-            res.send(drink);
-          }
+  Drink.find({ alcohol_tag: { $regex: ingredient, $options: 'i'}, tag: tag})
+    .limit(3)
+    .exec(function(err, responseDrink) {
+      if(err) {
+        res.send(500, {'error': err});
+      } else if (responseDrink.length === 0){
+        Drink.count({}, function(err, count) {
+          var rand = Math.ceil(Math.random() * count);
+          console.log("random drink");
+          Drink.findOne({random: rand}, function(err, drink) {
+            if (err) {
+              res.send({error: err});
+            }
+            else {
+              res.send(drink);
+            }
+          });
         });
-      });
-    }
-    else {
-      res.send(responseDrink);
-    }
+      }
+      else {
+        res.send(responseDrink);
+      }
   });
 };
 
