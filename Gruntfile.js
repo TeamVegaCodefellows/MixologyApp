ef80491a55c6824637e3166bc07eaa70c2af6276
bl00m <tguchi3@gmail.com>
2014-04-04 12:51:28 -0700

    added random drink get and added alcohol tag to drink seeds

--- Gruntfile.js
+++ Gruntfile.js
@@ -245,12 +245,12 @@ module.exports = function (grunt) {
         },
     });
 
-	grunt.registerTask('default',['express:dev', 'watch:express']);
+	  grunt.registerTask('default',['express:dev', 'watch:express']);
     grunt.registerTask('server', ['build:dev', 'express:dev','watch:all']);
     grunt.registerTask('test', ['env:dev', 'mongo_drop', 'mongoimport', 'mochacov:unit', 'mochacov:coverage']);
     grunt.registerTask('test:acceptance',['build:dev', 'express:dev', 'casper']);
     grunt.registerTask('travis', ['mochacov:unit', 'mochacov:coverage', 'mochacov:coveralls']);
-    grunt.registerTask('build:dev', ['clean:dev', 'sass:dev', 'browserify:dev', 'copy:dev']);
+    grunt.registerTask('build:dev', ['clean:dev', 'sass:dev', 'copy:dev', 'browserify:dev']);
     grunt.registerTask('server:notest', ['mongoimport', 'build:dev', 'express:dev', 'watch:notest']);
 
 };
