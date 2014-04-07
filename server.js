eb9516a741c90804d37aad91d02bdc6322f6ce86
ionox0 <ionox0@gmail.com>
2014-04-06 19:02:15 -0700

    testing removal of cors middleware

--- server.js
+++ server.js
@@ -12,22 +12,22 @@ app.engine('hbs', cons.handlebars);
 app.set('view engine', 'hbs')
 app.set('views', __dirname + '/app/templates');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
+//     // intercept OPTIONS method
+//     if ('OPTIONS' == req.method) {
+//       res.send(200);
+//     }
+//     else {
+//       next();
+//     }
+// };
 
 app.configure(function () {
-    app.use(allowCrossDomain);          //for MongoLabs connection
+    //app.use(allowCrossDomain);          //for MongoLabs connection
     app.use(express.static(__dirname + '/build'));
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
