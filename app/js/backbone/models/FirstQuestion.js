89cf061bbc5587a40911442c22d47a0370907dfd
ionox0 <ionox0@gmail.com>
2014-04-07 01:11:42 -0700

    using ianjohnson.co for api routes

--- app/js/backbone/models/FirstQuestion.js
+++ app/js/backbone/models/FirstQuestion.js
@@ -1,5 +1,5 @@
 module.exports = Backbone.Model.extend({
-	url: "http://54.186.126.16:3000/api/v1/getFirstQuestion/",
+	url: "http://ianjohnson.co:3000/api/v1/getFirstQuestion/",
 	defaults: {
 		"question" : "",
 		"choices" : [
