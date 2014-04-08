0208bb0118cbe6be14d3b810a97037a50731adc9
Ewelina <ewelina.kieley@gmail.com>
2014-04-08 14:01:10 -0700

    fixing my merge mess-up

--- app/js/backbone/main.js
+++ app/js/backbone/main.js
@@ -3,19 +3,4 @@ var Routes = require('./routers/Routes.js');
 $(function() {
 	var routes = new Routes();
 	Backbone.history.start();
-    window.fbAsyncInit = function() {
-            FB.init({
-              appId      : 239738456213562,
-              status     : true,
-              xfbml      : true
-            });
-          };
-          
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
 });
